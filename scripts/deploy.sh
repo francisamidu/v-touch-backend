dcc5c8cba47d06583026a070cc19ce046a3b1e76
Francis Amidu <francisamidu124@gmail.com>
2025-07-22 17:40:10 +0200

    Changed deploy config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# Detect Strapi Cloud via CI env
-if [ "$CI" = "strapi-cloud" ] || [ "$STRAPI_CLOUD" = "true" ]; then
-  echo "💡 Running npm install for Strapi Cloud"
+if [ "$CI" = "strapi-cloud" ]; then
   npm ci
 else
-  echo "🔧 Running pnpm install locally"
   pnpm install
 fi
 
-# Common build steps
 npm run build
 npm run strapi deploy
